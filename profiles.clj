3e5b7d3fe5592891a7b1efae2b8261cd606be1c9
Esko Luontola <esko.luontola@gmail.com>
2015-11-06 14:42:28 +0200

    Ignore profiles.clj

--- profiles.clj
+++
@@ -1,2 +0,0 @@
-{:profiles/dev  {:env {:database-url "jdbc:postgresql://localhost/territory_bro_dev?user=db_user_name_here&password=db_user_password_here"}}
- :profiles/test {:env {:database-url "jdbc:postgresql://localhost/territory_bro_test?user=db_user_name_here&password=db_user_password_here"}}}
