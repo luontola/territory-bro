9323a144e5791e768c464af55579a72cc4ce1599
Esko Luontola <esko@luontola.fi>
2016-05-06 21:08:30 +0300

    Fix no DB connection when running tests

--- test/territory_bro/handler_test.clj
+++ test/territory_bro/handler_test.clj
@@ -1,7 +1,16 @@
 (ns territory-bro.handler-test
   (:require [clojure.test :refer :all]
             [ring.mock.request :refer :all]
-            [territory-bro.handler :refer :all]))
+            [territory-bro.handler :refer :all]
+            [territory-bro.db.migrations :as migrations]
+            [territory-bro.db.core :as db]))
+
+(use-fixtures
+  :once
+  (fn [f]
+    (db/connect!)
+    (migrations/migrate ["migrate"])
+    (f)))
 
 (deftest test-app
   (testing "main route"
