27ae0dffe401ab43a549c24b2984e801d38f03ca
Esko Luontola <esko@luontola.fi>
2016-09-16 12:37:38 +0200

    Auth0 spike: remember login

--- web/index.js
+++ web/index.js
@@ -5,17 +5,35 @@ var lock = new Auth0Lock('8tVkdfnw8ynZ6rXNndD6eZ6ErsHdIgPi', 'luontola.eu.auth0.
 
 lock.on("authenticated", function(authResult) {
   console.log("authResult", authResult);
-  lock.getProfile(authResult.idToken, function(error, profile) {
-    if (error) {
-      console.log("auth error", error);
-      return;
-    }
-    console.log("profile", profile);
-    localStorage.setItem('id_token', authResult.idToken);
-  });
+  loadUserProfile(authResult.idToken)
 });
 
 var btn_login = document.getElementById('btn-login');
 btn_login.addEventListener('click', function() {
   lock.show();
 });
+
+function loadUserProfile(idToken) {
+  if (!idToken) {
+    idToken = localStorage.getItem('id_token');
+  }
+  if (!idToken) {
+    return;
+  }
+  lock.getProfile(idToken, function(error, profile) {
+    if (error) {
+      console.log("auth error", error);
+      return;
+    }
+    console.log("profile", profile);
+    localStorage.setItem('id_token', idToken);
+    showUserProfile(profile);
+  });
+}
+
+function showUserProfile(profile) {
+  var description = "Logged in as " + profile.name + " (" + profile.email + ", " + profile.user_id + ")";
+  document.getElementById('status').value = description;
+};
+
+loadUserProfile();
