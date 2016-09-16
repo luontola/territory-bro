99c48be73b7f031607a8e78a937e2e4e131b9b8f
Esko Luontola <esko@luontola.fi>
2016-09-16 12:20:40 +0200

    Auth0 spike: login

--- web/index.js
+++ web/index.js
@@ -1,2 +1,21 @@
 require("./style.css");
-document.write("It works.");
+var Auth0Lock = require("auth0-lock").default;
+
+var lock = new Auth0Lock('8tVkdfnw8ynZ6rXNndD6eZ6ErsHdIgPi', 'luontola.eu.auth0.com');
+
+lock.on("authenticated", function(authResult) {
+  console.log("authResult", authResult);
+  lock.getProfile(authResult.idToken, function(error, profile) {
+    if (error) {
+      console.log("auth error", error);
+      return;
+    }
+    console.log("profile", profile);
+    localStorage.setItem('id_token', authResult.idToken);
+  });
+});
+
+var btn_login = document.getElementById('btn-login');
+btn_login.addEventListener('click', function() {
+  lock.show();
+});
