9245d1d072127b421ceed494eb579e465b61f999
Esko Luontola <esko@luontola.fi>
2017-05-29 19:17:39 +0300

    Unit tests for getSelectedRegions and getSelectedTerritories

--- web/js/prints/PrintOptionsForm.test.js
+++ web/js/prints/PrintOptionsForm.test.js
@@ -4,34 +4,104 @@
 
 /* @flow */
 
-import {describe, it} from "mocha";
+import {beforeEach, describe, it} from "mocha";
 import {expect} from "chai";
-import {defaultMapRasterId, formName, getSelectedMapRaster} from "./PrintOptionsForm";
+import {
+  defaultMapRasterId,
+  formName,
+  getSelectedMapRaster,
+  getSelectedRegions,
+  getSelectedTerritories
+} from "./PrintOptionsForm";
 import reducers from "../reducers";
 import createStore from "redux/lib/createStore";
 import {mapRastersLoaded} from "../configActions";
 import {change} from "redux-form/lib/actions";
+import {regionsLoaded, territoriesLoaded} from "../apiActions";
+import type {Region, Territory} from "../api";
+import type {MapRaster} from "../maps/mapOptions";
 
 describe("PrintOptionsForm", () => {
+  let store;
+  beforeEach(() => {
+    store = createStore(reducers);
+  });
 
   describe("#getSelectedMapRaster", () => {
-    const defaultRaster = {id: defaultMapRasterId, name: "", source: null};
-    const otherRaster = {id: 'other', name: "", source: null};
+    const defaultRaster = rasterStub({id: defaultMapRasterId});
+    const otherRaster = rasterStub({id: 'other'});
     const mapRasters = [defaultRaster, otherRaster];
-
-    it("default value", () => {
-      const store = createStore(reducers);
+    beforeEach(() => {
       store.dispatch(mapRastersLoaded(mapRasters));
+    });
 
+    it("defaults to default raster", () => {
       expect(getSelectedMapRaster(store.getState())).to.eql(defaultRaster);
     });
 
-    it("changed value", () => {
-      const store = createStore(reducers);
-      store.dispatch(mapRastersLoaded(mapRasters));
+    it("changes with form selection", () => {
       store.dispatch(change(formName, 'mapRaster', otherRaster.id));
-
       expect(getSelectedMapRaster(store.getState())).to.eql(otherRaster);
     });
   });
+
+  describe("#getSelectedRegions", () => {
+    const region1 = regionStub({id: 1});
+    const region2 = regionStub({id: 2});
+    const region3 = regionStub({id: 3});
+    const regions = [region1, region2, region3];
+    beforeEach(() => {
+      store.dispatch(regionsLoaded(regions));
+    });
+
+    it("defaults to all regions", () => {
+      expect(getSelectedRegions(store.getState())).to.eql(regions);
+    });
+
+    it("changes with form selection", () => {
+      store.dispatch(change(formName, 'regions', ['1', '3']));
+      expect(getSelectedRegions(store.getState())).to.eql([region1, region3]);
+    });
+
+    it("empty selection implies all regions", () => {
+      store.dispatch(change(formName, 'regions', []));
+      expect(getSelectedRegions(store.getState())).to.eql(regions);
+    });
+  });
+
+  describe("#getSelectedTerritories", () => {
+    const territory1 = territoryStub({id: 1});
+    const territory2 = territoryStub({id: 2});
+    const territory3 = territoryStub({id: 3});
+    const territories = [territory1, territory2, territory3];
+    beforeEach(() => {
+      store.dispatch(territoriesLoaded(territories));
+    });
+
+    it("defaults to all territories", () => {
+      expect(getSelectedTerritories(store.getState())).to.eql(territories);
+    });
+
+    it("changes with form selection", () => {
+      store.dispatch(change(formName, 'territories', ['1', '3']));
+      expect(getSelectedTerritories(store.getState())).to.eql([territory1, territory3]);
+    });
+
+    it("empty selection implies all territories", () => {
+      store.dispatch(change(formName, 'territories', []));
+      expect(getSelectedTerritories(store.getState())).to.eql(territories);
+    });
+  });
 });
+
+function rasterStub(mapRaster: any): MapRaster {
+  return mapRaster;
+}
+
+function regionStub(region: any): Region {
+  return region;
+}
+
+function territoryStub(territory: any): Territory {
+  return territory;
+}
