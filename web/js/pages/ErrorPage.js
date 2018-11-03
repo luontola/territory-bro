930a18c6b8568fb0df5665135ef3a11e9146d4aa
Esko Luontola <esko@luontola.fi>
2018-11-03 20:56:23 +0200

    Move layout specific files to their own directory

--- web/js/pages/ErrorPage.js
+++ web/js/pages/ErrorPage.js
@@ -1,11 +1,11 @@
-// Copyright © 2015-2017 Esko Luontola
+// Copyright © 2015-2018 Esko Luontola
 // This software is released under the Apache License 2.0.
 // The license text is at http://www.apache.org/licenses/LICENSE-2.0
 
 /* @flow */
 
 import React from "react";
-import Layout from "./Layout";
+import Layout from "../layout/Layout";
 import type {ErrorMessage} from "../router";
 
 function formatErrorMessage(error) {
