aa9e71bc3e9393e725b3a22dff908e5fb2bde817
Esko Luontola <esko@luontola.fi>
2018-11-18 01:46:05 +0200

    Update instructions

--- web/js/pages/OverviewPage.js
+++ web/js/pages/OverviewPage.js
@@ -23,19 +23,13 @@ let OverviewPage = ({territoryCount, regionCount, loggedIn}: Props) => (
 
     <p>For more information, see <a href="http://territorybro.com">http://territorybro.com</a></p>
 
-    <h2>System Requirements</h2>
-
-    <p>The territory card UI uses <a href="http://caniuse.com/#feat=css-variables">CSS varibles</a> and <a
-      href="http://caniuse.com/#feat=css-grid">Grid layout</a>, which might not be supported by all web browsers.
-      Use the latest version of Chrome or Firefox for best results. There are also differences in the image quality
-      of PDFs, so compare various web browsers before printing your territory cards.</p>
-
     <h2>Import</h2>
 
     <p>The database has currently {territoryCount} territories and {regionCount} regions.</p>
 
     {loggedIn ?
-      <p>Importing is disabled.</p>
+      <p>Importing is disabled when logged in. You can now print your congregation territory cards directly,
+        without the need for an import step.</p>
       :
       <React.Fragment>
         <form action="/api/clear-database" method="post">
