460d9251299a97c7b074473706a10afa9adbc9e8
Esko Luontola <esko@luontola.fi>
2019-04-22 01:01:14 +0300

    New schema for territories and regions
    - Split the region table to congregation_boundary, subregion and
      card_minimap_viewport tables
    - Use UUIDs
    - Use MultiPolygons

--- resources/db/flyway/tenant/V1__territory_region_tables.sql
+++ resources/db/flyway/tenant/V1__territory_region_tables.sql
@@ -1,18 +1,27 @@
 create table territory
 (
-    id       serial primary key not null,
-    number   varchar(100)       not null default '',
-    address  text               not null default '',
-    region   varchar(1000)      not null default '',
-    location geography(polygon, 4326)
+    id        uuid primary key,
+    number    varchar(100)  not null default '',
+    addresses varchar(1000) not null default '',
+    subregion varchar(1000) not null default '',
+    location  geography(multipolygon, 4326)
 );
 
-create table region
+create table congregation_boundary
 (
-    id               serial primary key not null,
-    name             varchar(1000)      not null default '',
-    location         geography(polygon, 4326),
-    minimap_viewport boolean            not null default false,
-    congregation     boolean            not null default false,
-    subregion        boolean            not null default false
+    id       uuid primary key,
+    location geography(multipolygon, 4326)
+);
+
+create table subregion
+(
+    id       uuid primary key,
+    name     varchar(1000) not null default '',
+    location geography(multipolygon, 4326)
+);
+
+create table card_minimap_viewport
+(
+    id       uuid primary key,
+    location geography(polygon, 4326)
 );
