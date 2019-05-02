648d295a3072be19a5161be34265e0719e612fe8
Esko Luontola <esko@luontola.fi>
2019-05-03 02:38:01 +0300

    Placeholder test to avoid build failing due to lack of tests

---
+++ web/js/example.test.js
@@ -0,0 +1,17 @@
+// Copyright © 2015-2019 Esko Luontola
+// This software is released under the Apache License 2.0.
+// The license text is at http://www.apache.org/licenses/LICENSE-2.0
+
+/* @flow */
+
+import {beforeEach, describe, it} from "mocha";
+import {expect} from "chai";
+
+describe("ExampleTest", () => {
+  beforeEach(() => {
+  });
+
+  it("demonstrates test syntax", () => {
+    expect(true).to.eql(true);
+  });
+});
