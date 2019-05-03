098a6ca1bce77c5d6ff023440b8dce57dc9c669a
Esko Luontola <esko@luontola.fi>
2019-05-04 00:57:59 +0300

    Data migration to new schema

---
+++ src/territory_bro/migration.clj
@@ -0,0 +1,42 @@
+;; Copyright © 2015-2019 Esko Luontola
+;; This software is released under the Apache License 2.0.
+;; The license text is at http://www.apache.org/licenses/LICENSE-2.0
+
+(ns territory-bro.migration
+  (:require [clojure.tools.logging :as log]
+            [territory-bro.config :as config]
+            [territory-bro.congregation :as congregation]
+            [territory-bro.db :as db]
+            [territory-bro.gis-user :as gis-user]
+            [territory-bro.region :as region]
+            [territory-bro.territory :as territory]
+            [territory-bro.user :as user]))
+
+(defn migrate-congregation [tenant]
+  (db/as-tenant tenant
+    (let [regions (db/query :find-regions)
+          territories (db/query :find-territories)
+          admins (get-in config/env [:tenant tenant :admins])]
+      (db/with-db [conn {}] ; to be able to see the new schema
+        (let [cong-id (congregation/create-congregation! conn (.toUpperCase (name tenant)))]
+
+          (doseq [admin admins]
+            (let [user-id (user/save-user! conn admin {})]
+              (congregation/grant-access! conn cong-id user-id)
+              (gis-user/create-gis-user! conn cong-id user-id)))
+
+          (congregation/use-schema conn cong-id)
+
+          (doseq [region regions]
+            (cond
+              (:congregation region) (region/create-congregation-boundary! conn (:location region))
+              (:subregion region) (region/create-subregion! conn (:name region) (:location region))
+              (:minimap_viewport region) (region/create-card-minimap-viewport! conn (:location region))
+              :else (region/create-subregion! conn (:name region) (:location region))))
+
+          (doseq [territory territories]
+            (territory/create-territory! conn {::territory/number (:number territory)
+                                               ::territory/addresses (:address territory)
+                                               ::territory/subregion (:region territory)
+                                               ::territory/location (:location territory)}))))))
+  (log/info "Migrated congregation" tenant))
