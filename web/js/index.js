5e1cfafd2859f1ad7a6fb214caa62503edf30e3c
Esko Luontola <esko@luontola.fi>
2019-05-04 02:35:44 +0300

    Error boundary to show uncaught exceptions

--- web/js/index.js
+++ web/js/index.js
@@ -11,6 +11,7 @@ import {createLogger} from "redux-logger";
 import reducers from "./reducers";
 import {IntlProvider} from "react-intl";
 import {language, messages} from "./intl";
+import ErrorBoundary from "react-error-boundary";
 import OverviewPage from "./pages/OverviewPage";
 import CongregationPage from "./pages/CongregationPage";
 import RegistrationPage from "./pages/RegistrationPage";
@@ -30,26 +31,35 @@ globalHistory.listen(({location, action}) => {
   console.info(`Current URL is now ${location.pathname}${location.search}${location.hash} (${action})`);
 });
 
+const ErrorPage = ({componentStack, error}) => (
+  <>
+    <h1>Sorry, something went wrong 🥺</h1>
+    <pre>{`${error.stack}\n\nThe error is located at:${componentStack}`}</pre>
+  </>
+);
+
 root.render(
   <React.StrictMode>
-    <IntlProvider locale={language} messages={messages}>
-      <Provider store={store}>
-        <React.Suspense fallback={<p>Loading....</p>}>
-          <Layout>
-            <Router>
-              <OverviewPage path="/"/>
-              <RegistrationPage path="/register"/>
-              <LoginCallbackPage path="/login-callback"/>
-              <CongregationPage path="/congregation/:congregationId"/>
-              <TerritoryCardsPage path="/congregation/:congregationId/territory-cards"/>
-              <NeighborhoodCardsPage path="/congregation/:congregationId/neighborhood-maps"/>
-              <RuralTerritoryCardsPage path="/congregation/:congregationId/rural-territory-cards"/>
-              <RegionPrintoutsPage path="/congregation/:congregationId/region-maps"/>
-              <NotFoundPage default/>
-            </Router>
-          </Layout>
-        </React.Suspense>
-      </Provider>
-    </IntlProvider>
+    <ErrorBoundary FallbackComponent={ErrorPage}>
+      <IntlProvider locale={language} messages={messages}>
+        <Provider store={store}>
+          <React.Suspense fallback={<p>Loading....</p>}>
+            <Layout>
+              <Router>
+                <OverviewPage path="/"/>
+                <RegistrationPage path="/register"/>
+                <LoginCallbackPage path="/login-callback"/>
+                <CongregationPage path="/congregation/:congregationId"/>
+                <TerritoryCardsPage path="/congregation/:congregationId/territory-cards"/>
+                <NeighborhoodCardsPage path="/congregation/:congregationId/neighborhood-maps"/>
+                <RuralTerritoryCardsPage path="/congregation/:congregationId/rural-territory-cards"/>
+                <RegionPrintoutsPage path="/congregation/:congregationId/region-maps"/>
+                <NotFoundPage default/>
+              </Router>
+            </Layout>
+          </React.Suspense>
+        </Provider>
+      </IntlProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
