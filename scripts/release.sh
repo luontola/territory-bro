b5c19779535fc62ce37bf2ea91d565598ee6d43f
Esko Luontola <esko@luontola.fi>
2019-08-12 01:05:42 +0300

    🚨 Fix code inspection warnings

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eu
 #: ${1:? Usage: $0 RELEASE_VERSION}
-DEFAULT_TAG=`date +%Y-%m-%d`
+DEFAULT_TAG=$(date +%Y-%m-%d)
 TAG="${1:-$DEFAULT_TAG}"
-echo TAG=$TAG
+echo TAG="$TAG"
 set -x
 
 docker-compose build --pull
