52947faa8397dd6bbe72697725b0754d5fb7c3e9
Esko Luontola <esko@luontola.fi>
2019-08-27 22:55:14 +0300

    🚸 Link back to congregation page

--- web/js/pages/PrintoutPage.js
+++ web/js/pages/PrintoutPage.js
@@ -4,12 +4,15 @@
 
 import React from "react";
 import PrintOptionsForm from "../prints/PrintOptionsForm";
+import {getCongregationById} from "../api";
+import {Link} from "@reach/router";
 
 const PrintoutPage = ({congregationId}) => {
+  const congregation = getCongregationById(congregationId);
   return (
     <>
       <div className="no-print">
-        <h1>Printouts</h1>
+        <h1><Link to="..">{congregation.name}</Link>: Printouts</h1>
       </div>
       <PrintOptionsForm congregationId={congregationId}/>
     </>
