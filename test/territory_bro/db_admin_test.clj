866264aa7a9f0bf2713e66daf0929a4ad07f1d85
Esko Luontola <esko@luontola.fi>
2019-09-07 13:46:03 +0300

    ♻️ Move test data to top level

--- test/territory_bro/db_admin_test.clj
+++ test/territory_bro/db_admin_test.clj
@@ -8,43 +8,43 @@
             [territory-bro.testutil :as testutil])
   (:import (java.util UUID)))
 
+(def cong-id (UUID. 0 1))
+(def user-id (UUID. 0 2))
+(def cong-created-event {:event/type :congregation.event/congregation-created
+                         :event/version 1
+                         :congregation/id cong-id
+                         :congregation/name "Cong1 Name"
+                         :congregation/schema-name "cong1_schema"})
+(def user-created-event {:event/type :congregation.event/gis-user-created
+                         :event/version 1
+                         :congregation/id cong-id
+                         :user/id user-id
+                         :gis-user/username "username123"
+                         :gis-user/password "password123"})
+(def user-deleted-event {:event/type :congregation.event/gis-user-deleted
+                         :event/version 1
+                         :congregation/id cong-id
+                         :user/id user-id
+                         :gis-user/username "username123"})
+(def user-is-present-event {:event/type :db-admin.event/gis-user-is-present
+                            :event/version 1
+                            :event/transient? true
+                            :congregation/id cong-id
+                            :user/id user-id
+                            :gis-user/username "username123"})
+(def user-is-absent-event {:event/type :db-admin.event/gis-user-is-absent
+                           :event/version 1
+                           :event/transient? true
+                           :congregation/id cong-id
+                           :user/id user-id
+                           :gis-user/username "username123"})
+
 (defn- apply-events [events]
   (testutil/apply-events db-admin/projection events))
 
 (deftest projection-test
   (testing "congregation created"
-    (let [cong-id (UUID. 0 1)
-          user-id (UUID. 0 2)
-          cong-created-event {:event/type :congregation.event/congregation-created
-                              :event/version 1
-                              :congregation/id cong-id
-                              :congregation/name "Cong1 Name"
-                              :congregation/schema-name "cong1_schema"}
-          user-created-event {:event/type :congregation.event/gis-user-created
-                              :event/version 1
-                              :congregation/id cong-id
-                              :user/id user-id
-                              :gis-user/username "username123"
-                              :gis-user/password "password123"}
-          user-deleted-event {:event/type :congregation.event/gis-user-deleted
-                              :event/version 1
-                              :congregation/id cong-id
-                              :user/id user-id
-                              :gis-user/username "username123"}
-          user-is-present-event {:event/type :db-admin.event/gis-user-is-present
-                                 :event/version 1
-                                 :event/transient? true
-                                 :congregation/id cong-id
-                                 :user/id user-id
-                                 :gis-user/username "username123"}
-          user-is-absent-event {:event/type :db-admin.event/gis-user-is-absent
-                                :event/version 1
-                                :event/transient? true
-                                :congregation/id cong-id
-                                :user/id user-id
-                                :gis-user/username "username123"}
-
-          events [cong-created-event]
+    (let [events [cong-created-event]
           expected {::db-admin/congregations {cong-id {:congregation/schema-name "cong1_schema"}}
                     ::db-admin/pending-tenants #{{:congregation/schema-name "cong1_schema"}}}]
       (is (= expected (apply-events events)))
