2561cb15ded376e9678765379f9fdeb733ac05a9
Esko Luontola <esko@luontola.fi>
2020-02-08 01:28:45 +0200

    ⬆️ Upgrade all Clojure dependencies

--- project.clj
+++ project.clj
@@ -7,39 +7,39 @@
   :description "Territory Bro is a tool for managing territory cards in the congregations of Jehovah's Witnesses."
   :url "https://territorybro.com"
 
-  :dependencies [[camel-snake-kebab "0.4.0"]
+  :dependencies [[camel-snake-kebab "0.4.1"]
                  [com.attendify/schema-refined "0.3.0-alpha4"]
-                 [com.auth0/java-jwt "3.8.2"]
-                 [com.auth0/jwks-rsa "0.8.3"]
-                 [com.fasterxml.jackson.core/jackson-core "2.9.9"]
-                 [com.fasterxml.jackson.core/jackson-databind "2.9.9.1"]
-                 [com.fasterxml.jackson.datatype/jackson-datatype-jsr310 "2.9.9"]
-                 [com.layerware/hugsql "0.4.9"]
+                 [com.auth0/java-jwt "3.9.0"]
+                 [com.auth0/jwks-rsa "0.9.0"]
+                 [com.fasterxml.jackson.core/jackson-core "2.10.2"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.10.2"]
+                 [com.fasterxml.jackson.datatype/jackson-datatype-jsr310 "2.10.2"]
+                 [com.layerware/hugsql "0.5.1"]
                  [compojure "1.6.1"]
-                 [conman "0.8.3"]
-                 [cprop "0.1.14"]
+                 [conman "0.8.4"]
+                 [cprop "0.1.15"]
                  [liberator "0.15.3"]
                  [luminus-immutant "0.2.5"]
                  [luminus-nrepl "0.1.6"]
                  [medley "1.2.0"]
-                 [metosin/jsonista "0.2.4"]
+                 [metosin/jsonista "0.2.5"]
                  [metosin/ring-http-response "0.9.1"]
                  [metosin/ring-middleware-format "0.6.0"] ;; TODO: replace with newer library
-                 [metosin/schema-tools "0.12.0"]
+                 [metosin/schema-tools "0.12.2"]
                  [mount "0.1.16"]
                  [org.clojars.luontola/ns-tracker "0.3.1-patch1"]
                  [org.clojure/clojure "1.10.1"]
-                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/data.json "0.2.7"]
                  [org.clojure/test.check "0.10.0"]
                  [org.clojure/tools.cli "0.4.2"]
-                 [org.clojure/tools.logging "0.5.0"]
+                 [org.clojure/tools.logging "0.6.0"]
                  [org.clojure/tools.reader "1.3.2"] ;; XXX: overrides old version from metosin/ring-middleware-format
-                 [org.flywaydb/flyway-core "6.0.1"]
-                 [org.postgresql/postgresql "42.2.6"]
+                 [org.flywaydb/flyway-core "6.2.2"]
+                 [org.postgresql/postgresql "42.2.10"]
                  [prismatic/schema "1.1.12"]
                  [prismatic/schema-generators "0.1.3"]
                  [ring-logger "1.0.1"]
-                 [ring/ring-core "1.7.1"]
+                 [ring/ring-core "1.8.0"]
                  [ring/ring-defaults "0.3.2"]]
 
   :min-lein-version "2.0.0"
@@ -64,7 +64,7 @@
                        :aot :all
                        :uberjar-name "territory-bro.jar"}
 
-             :dev {:dependencies [[ring/ring-devel "1.7.1" :exclusions [ns-tracker]]
+             :dev {:dependencies [[ring/ring-devel "1.8.0" :exclusions [ns-tracker]]
                                   [ring/ring-mock "0.4.0"]]
                    :jvm-opts ^:replace ["-Dconf=dev-config.edn"
                                         "-XX:-OmitStackTraceInFastThrow"
@@ -77,4 +77,4 @@
                                           "--illegal-access=deny"]}
 
              :kaocha [:test :kaocha0]
-             :kaocha0 {:dependencies [[lambdaisland/kaocha "0.0-581"]]}})
+             :kaocha0 {:dependencies [[lambdaisland/kaocha "0.0-590"]]}})
