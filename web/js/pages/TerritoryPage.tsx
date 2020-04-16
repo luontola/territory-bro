c9f1771bebae4ef8c7a519991268f5f3b1a2a1b9
Esko Luontola <esko@luontola.fi>
2020-04-17 02:01:56 +0300

    🚸 Talk about regions instead of subregions in the UI
    Why:
    - Region is already the established word in the UI
    - Subregion is a remainder of the time when region was an umbrella term
      for also congregation boundaries and minimap viewports
    - It might be a good idea to get rid of the subregion term also in the
      code, though to avoid breaking existing QGIS projects, changing the
      GIS tables should be avoided

--- web/js/pages/TerritoryPage.tsx
+++ web/js/pages/TerritoryPage.tsx
@@ -1,4 +1,4 @@
-// Copyright © 2015-2019 Esko Luontola
+// Copyright © 2015-2020 Esko Luontola
 // This software is released under the Apache License 2.0.
 // The license text is at http://www.apache.org/licenses/LICENSE-2.0
 
@@ -32,7 +32,7 @@ const TerritoryPage = ({congregationId, territoryId}) => {
               <td>{territory.number}</td>
             </tr>
             <tr>
-              <th>Subregion</th>
+              <th>Region</th>
               <td>{territory.subregion}</td>
             </tr>
             <tr>
