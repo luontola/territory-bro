26cac0f683c3fe755a5ef859d2967a599057cd58
Esko Luontola <esko@luontola.fi>
2020-04-16 22:38:13 +0300

    ✨ Filter search for territories
    Why:
    - Basic search for finding the right territory
    - Form submit had to be disabled to avoid pressing Enter triggering form
      submit and thus reloading the page
    - When the Clear button is pressed, the button itself disappears, so the
      focus is best moved to the text field, or no element would have focus

--- web/js/pages/TerritoriesPage.tsx
+++ web/js/pages/TerritoriesPage.tsx
@@ -2,15 +2,56 @@
 // This software is released under the Apache License 2.0.
 // The license text is at http://www.apache.org/licenses/LICENSE-2.0
 
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "@reach/router";
-import {getCongregationById} from "../api";
+import {getCongregationById, Territory} from "../api";
+import styles from "./TerritoriesPage.css";
+
+function SearchForm({search, setSearch}) {
+  const id = 'territory-search';
+  return (
+    <form className={styles.search + " pure-form"}
+          onSubmit={event => event.preventDefault()}>
+      <label htmlFor={id}>Search</label>
+      <input id={id}
+             type="text"
+             className="pure-input-rounded"
+             autoComplete="off"
+             value={search}
+             onChange={event => {
+               setSearch(event.target.value);
+             }}/>
+      {search !== '' &&
+      <button type="button"
+              className="pure-button"
+              onClick={() => {
+                setSearch('');
+                document.getElementById(id).focus();
+              }}>
+        Clear
+      </button>
+      }
+    </form>
+  );
+}
+
+function matchesSearch(territory: Territory, search: string): boolean {
+  if (search === '') {
+    return true;
+  }
+  const {number, subregion, addresses} = territory;
+  search = search.toLowerCase();
+  return number.toLowerCase().includes(search)
+    || subregion.toLowerCase().includes(search)
+    || addresses.toLowerCase().includes(search);
+}
 
 const TerritoriesPage = ({congregationId}) => {
   const congregation = getCongregationById(congregationId);
-  // TODO: filter search
+  const [search, setSearch] = useState('')
   return <>
     <h1><Link to="..">{congregation.name}</Link>: Territories</h1>
+    <SearchForm search={search} setSearch={setSearch}/>
     <table className="pure-table pure-table-striped">
       <thead>
       <tr>
@@ -20,13 +61,15 @@ const TerritoriesPage = ({congregationId}) => {
       </tr>
       </thead>
       <tbody>
-      {congregation.territories.map(territory =>
-        <tr key={territory.id}>
-          <td>{territory.number}</td>
-          <td>{territory.subregion}</td>
-          <td>{territory.addresses}</td>
-        </tr>
-      )}
+      {congregation.territories
+        .filter(territory => matchesSearch(territory, search))
+        .map(territory =>
+          <tr key={territory.id}>
+            <td>{territory.number}</td>
+            <td>{territory.subregion}</td>
+            <td>{territory.addresses}</td>
+          </tr>
+        )}
       </tbody>
     </table>
   </>;
