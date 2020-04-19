0b1807cb0309114df6b096d02549f0ae98da5fcf
Esko Luontola <esko@luontola.fi>
2020-04-19 06:58:40 +0300

    🚸 Pad the geolocation to avoid it being too close to the edge
    Why:
    - It can be hard to notice the geolocation indicator when it's right at
      the map edge, especially if it's very far away from the territory and
      the map needs to be zoomed far out

--- web/js/maps/ShowMyLocation.ts
+++ web/js/maps/ShowMyLocation.ts
@@ -3,7 +3,7 @@
 // The license text is at http://www.apache.org/licenses/LICENSE-2.0
 
 import Control from "ol/control/Control";
-import {buffer, extend} from "ol/extent";
+import {boundingExtent, buffer, containsCoordinate, extend, extendCoordinate, getHeight, getWidth} from "ol/extent";
 
 class ShowMyLocation extends Control {
   constructor(startGeolocation: (map: any) => void) {
@@ -21,27 +21,46 @@ class ShowMyLocation extends Control {
       event.preventDefault();
       const map = super.getMap();
 
-      // Start the geolocation tracking only once,
-      // but on every click zoom out to fit the user
+      // Start the geolocation tracking only once, but always zoom out to fit the user
       if (!geolocation) {
         geolocation = startGeolocation(map);
       }
 
-      function zoomOutToFit(geom) {
+      function zoomOutToFit(position) {
         const view = map.getView();
-        const extent = extend(view.calculateExtent(), buffer(geom.getExtent(), 20));
-        view.fit(extent, {duration: 500});
+        const extent = view.calculateExtent()
+
+        // TODO: calculate distance to nearest edge, and zoom out if too close to it
+        const paddedPosition1 = buffer(boundingExtent([position]), 30);
+        if (containsCoordinate(extent, paddedPosition1)) {
+          return; // point is already clearly inside; no need to zoom
+        }
+
+        // Pad the user position relative to the map size, to avoid
+        // it being too close to the edge where it's hard to notice.
+        extendCoordinate(extent, position);
+        // TODO: calculate distance to nearest edge, instead of assuming the map is square
+        const dimension = Math.min(getWidth(extent), getHeight(extent));
+        const paddedPosition2 = buffer(boundingExtent([position]), dimension * 0.5);
+        extend(extent, paddedPosition2);
+
+        // TODO: could also just use this padding here, instead of the above extent calculations,
+        //       but in that case the extent would need to be calculated from the territory layer,
+        //       so that the extent calculation is idempotent and re-clicking does not zoom further out
+        view.fit(extent, {
+          duration: 500,
+          padding: [20, 20, 20, 20], // same padding as in TerritoryMap's resetZoom for consistency
+        });
       }
 
-      // On first click we must wait asynchronously until
-      // the sensors return some geolocation data.
-      // On subsequent clicks we can use the latest known location.
-      const geom = geolocation.getAccuracyGeometry();
-      if (geom) {
-        zoomOutToFit(geom);
+      // On first click the position data is not yet available; need asynchrony.
+      // On subsequent clicks the position is cached and can be used synchronously.
+      const position = geolocation.getPosition();
+      if (position) {
+        zoomOutToFit(position);
       } else {
-        geolocation.once('change:accuracyGeometry', () => {
-          zoomOutToFit(geolocation.getAccuracyGeometry());
+        geolocation.once('change:position', () => {
+          zoomOutToFit(geolocation.getPosition());
         })
       }
     };
