94998d8ef54185621f2a08a2d27746f54fee9d47
Esko Luontola <esko@luontola.fi>
2020-04-22 23:17:59 +0300

    🚸 Navigation bar with nested congregation pages
    Why:
    - Making the site look less ascetic
    - Implementing a proper navigation menu

--- web/js/layout/Layout.tsx
+++ web/js/layout/Layout.tsx
@@ -5,45 +5,66 @@
 import "./defaultStyles";
 import styles from "./Layout.css";
 import * as React from "react";
+import {useEffect} from "react";
 import AuthenticationPanel from "./AuthenticationPanel";
-import {Link} from "@reach/router";
+import {Link, Router} from "@reach/router";
+import {getCongregationById} from "../api";
 
 type Props = {
   title?: string;
   children?: React.ReactNode;
 };
 
-class Layout extends React.Component<Props> {
+const HomeNav = ({}) => {
+  return (
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><a href="https://territorybro.com/guide/">User Guide</a></li>
+      <li><a href="https://groups.google.com/forum/#!forum/territory-bro-announcements">News</a></li>
+      <li><Link to="/help">Help</Link></li>
+    </ul>
+  );
+}
 
-  componentDidMount() {
+const CongregationNav = ({congregationId}) => {
+  const congregation = getCongregationById(congregationId);
+  return (
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li>{congregation.name}</li>
+      <ul>
+        <li><Link to="territories">Territories</Link></li>
+        <li><Link to="printouts">Printouts</Link></li>
+        {congregation.permissions.configureCongregation && <>
+          <li><Link to="users">Users</Link></li>
+          <li><Link to="settings">Settings</Link></li>
+        </>}
+      </ul>
+      <li><Link to="/help">Help</Link></li>
+    </ul>
+  );
+}
+
+const Layout = ({title, children}: Props) => {
+  useEffect(() => {
     // TODO: get title from H1
-    const {
-      title
-    } = this.props;
     const site = "Territory Bro";
     document.title = title ? `${title} - ${site}` : site;
-  }
+  }, []);
 
-  render() {
-    const {
-      children
-    } = this.props;
-    return <>
-      <nav className={`${styles.navigation} no-print`}>
-        <AuthenticationPanel/>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><a href="https://territorybro.com/guide/">User Guide</a></li>
-          <li><a href="https://groups.google.com/forum/#!forum/territory-bro-announcements">Updates</a></li>
-          <li><Link to="/help">Help</Link></li>
-        </ul>
-      </nav>
+  return <>
+    <nav className={`${styles.navigation} no-print`}>
+      <AuthenticationPanel/>
+      <Router>
+        <HomeNav path="/*"/>
+        <CongregationNav path="/congregation/:congregationId/*"/>
+      </Router>
+    </nav>
 
-      <main className={styles.content}>
-        {children}
-      </main>
-    </>;
-  }
+    <main className={styles.content}>
+      {children}
+    </main>
+  </>;
 }
 
 export default Layout;
