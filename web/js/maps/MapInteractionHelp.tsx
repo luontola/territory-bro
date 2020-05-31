dcfd4f5e54053f358e6504e944b888980c8b75da
Esko Luontola <esko@luontola.fi>
2020-06-01 01:54:24 +0300

    ♻️ Extract InfoBox
    Why:
    - Reusable component for showing help texts
    - Considered deduplicating this with the gray box of the share button
      popup, but there were quite many differences in their style and
      intended use, so it's safer to keep them separate to avoid unnecessary
      design pressure

--- web/js/maps/MapInteractionHelp.tsx
+++ web/js/maps/MapInteractionHelp.tsx
@@ -3,24 +3,18 @@
 // The license text is at http://www.apache.org/licenses/LICENSE-2.0
 
 import React from "react";
-import styles from "./MapInteractionHelp.css"
+import InfoBox from "./InfoBox";
 
 const MapInteractionHelp = () => {
   const ctrl = navigator.platform.startsWith('Mac') ? 'Cmd' : 'Ctrl';
-  return <div className={styles.root}>
-    <div className={styles.title}>
-      <i className="fas fa-info-circle"/> How to interact with the maps?
-    </div>
-
-    <div className={styles.content}>
-      <p><b>Pan:</b> drag with the left mouse button
-        / drag with two fingers<br/></p>
-      <p><b>Zoom:</b> hold <kbd>{ctrl}</kbd> and scroll with the mouse wheel
-        / pinch or spread with two fingers<br/></p>
-      <p><b>Rotate:</b> hold <kbd>Alt</kbd>+<kbd>Shift</kbd> and drag with the left mouse button
-        / rotate with two fingers</p>
-    </div>
-  </div>
+  return <InfoBox title={"How to interact with the maps?"}>
+    <p><b>Pan:</b> drag with the left mouse button
+      / drag with two fingers<br/></p>
+    <p><b>Zoom:</b> hold <kbd>{ctrl}</kbd> and scroll with the mouse wheel
+      / pinch or spread with two fingers<br/></p>
+    <p><b>Rotate:</b> hold <kbd>Alt</kbd>+<kbd>Shift</kbd> and drag with the left mouse button
+      / rotate with two fingers</p>
+  </InfoBox>
 };
 
 export default MapInteractionHelp;
