59bf97f6b1a0b74031384184774d87594308a02e
Esko Luontola <esko@luontola.fi>
2021-06-07 22:47:19 +0300

    🚸 Placeholder for missing territory numbers
    Why:
    - If a territory has no number, it's not possible to click it and
      navigate to the territory page.

--- web/js/pages/TerritoryListPage.tsx
+++ web/js/pages/TerritoryListPage.tsx
@@ -1,4 +1,4 @@
-// Copyright © 2015-2020 Esko Luontola
+// Copyright © 2015-2021 Esko Luontola
 // This software is released under the Apache License 2.0.
 // The license text is at http://www.apache.org/licenses/LICENSE-2.0
 
@@ -83,7 +83,7 @@ const TerritoryListPage = ({congregationId}) => {
         .map(territory =>
           <tr key={territory.id}>
             <td className={styles.number}>
-              <Link to={territory.id}>{territory.number}</Link>
+              <Link to={territory.id}>{territory.number === '' ? '-' : territory.number}</Link>
             </td>
             <td>{territory.region}</td>
             <td>{territory.addresses}</td>
