f21c165e759a19ba389e4792d74ff4dce14e7962
Esko Luontola <esko@luontola.fi>
2023-08-06 21:04:26 +0300

    🔥 Remove dead code

--- web/src/global.d.ts
+++
@@ -1,8 +0,0 @@
-// Copyright © 2015-2020 Esko Luontola
-// This software is released under the Apache License 2.0.
-// The license text is at http://www.apache.org/licenses/LICENSE-2.0
-
-declare module '*.css' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
