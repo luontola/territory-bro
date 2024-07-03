6ecaaf4ccf213db87aaf0aadf589a6765cef6fea
Esko Luontola <esko@luontola.fi>
2024-07-03 20:27:13 +0300

    ♻️ Extract territory-bro.ui.printout-templates
    Why:
    - Simplifies declaration order issues on the printouts-page namespace.
    - There will be many more templates and keeping them separate makes the
      code more cohesive. It's not yet sure whether to put each template in
      its own namespace, or whether it's more cohesive to keep them together.

--- src/territory_bro/ui/printouts_page.clj
+++ src/territory_bro/ui/printouts_page.clj
@@ -8,11 +8,11 @@
             [territory-bro.api :as api]
             [territory-bro.infra.json :as json]
             [territory-bro.infra.resources :as resources]
-            [territory-bro.ui.css :as css]
             [territory-bro.ui.html :as html]
             [territory-bro.ui.i18n :as i18n]
             [territory-bro.ui.layout :as layout]
-            [territory-bro.ui.map-interaction-help :as map-interaction-help])
+            [territory-bro.ui.map-interaction-help :as map-interaction-help]
+            [territory-bro.ui.printout-templates :as printout-templates])
   (:import (net.greypanther.natsort CaseInsensitiveSimpleNaturalComparator)))
 
 (def templates
@@ -68,58 +68,6 @@
                    (update :territories parse-uuid-multiselect))}
         (merge (map-interaction-help/model request)))))
 
-
-(defn crop-marks [content]
-  (let [styles (:CropMarks (css/modules))
-        image (h/html [:img {:src (get html/public-resources "/assets/crop-mark-*.svg")
-                             :alt ""}])]
-    (h/html
-     [:div {:class (:root styles)}
-      [:div {:class (:topLeft styles)} image]
-      [:div {:class (:topRight styles)} image]
-      [:div {:class (:cropArea styles)} content]
-      [:div {:class (:bottomLeft styles)} image]
-      [:div {:class (:bottomRight styles)} image]])))
-
-(defn territory-card [territory]
-  (let [styles (:TerritoryCard (css/modules))]
-    (crop-marks
-     (h/html
-      [:div {:class (:root styles)}
-
-       [:div {:class (:minimap styles)}
-        ;; TODO: map using web components
-        [:div.OpenLayersMap__root--f9d8701d.OpenLayersMap__printout--f9d8701d
-         [:div.ol-viewport {:style "position: relative; overflow: hidden; width: 100%; height: 100%;"}
-          "[OpenLayersMap]"]]]
-
-       [:div {:class (:header styles)}
-        [:div {:class (:title styles)} (i18n/t "TerritoryCard.title")]
-        [:div {:class (:region styles)} (:region territory)]]
-
-       [:div {:class (:number styles)} (:number territory)]
-
-       [:div {:class (:map styles)}
-        [:div.PrintDateNotice__root--5851a1da
-         ;; TODO: find out timezone for current date based on congregation location
-         ;;       https://stackoverflow.com/questions/16086962/how-to-get-a-time-zone-from-a-location-using-latitude-and-longitude-coordinates/16086964#16086964
-         ;;       https://github.com/RomanIakovlev/timeshape
-         [:div.PrintDateNotice__notice--5851a1da "Printed 2024-05-12 with TerritoryBro.com"]
-         [:div.PrintDateNotice__content--5851a1da
-          ;; TODO: map using web components
-          [:div.OpenLayersMap__root--f9d8701d.OpenLayersMap__printout--f9d8701d
-           [:div.ol-viewport {:style "position: relative; overflow: hidden; width: 100%; height: 100%;"}
-            "[OpenLayersMap]"]]]]]
-
-       [:div {:class (:addresses styles)}
-        [:div {:class (:qrCode styles)}
-         ;; TODO: QR code using lazy loading with caching
-         [:svg {:height "128" :width "128" :viewBox "0 0 29 29" :style "width: 100%; height: auto;"}
-          [:path {:fill "#FFFFFF" :d "M0,0 h29v29H0z" :shape-rendering "crispEdges"}]
-          [:path {:fill "#000000" :d "M0 0h7v1H0zM11 0h1v1H11zM14 0h1v1H14zM18 0h1v1H18zM22,0 h7v1H22zM0 1h1v1H0zM6 1h1v1H6zM8 1h2v1H8zM13 1h1v1H13zM17 1h1v1H17zM22 1h1v1H22zM28,27 h1v1H28zM0 28h7v1H0zM8 28h1v1H8zM10 28h3v1H10zM14 28h2v1H14zM19 28h1v1H19zM24 28h3v1H24z" :shape-rendering "crispEdges"}]]]
-        (:addresses territory)]
-
-       [:div {:class (:footer styles)} (i18n/t "TerritoryCard.footer")]]))))
 
 (defn view [{:keys [congregation territories regions form] :as model}]
   (let [printout-lang (i18n/validate-lang (keyword (:language form)))]
@@ -196,7 +144,7 @@
             :when (contains? (:territories form) (:id territory))]
         (binding [i18n/*lang* printout-lang]
           ;; TODO: conditional based on selected template
-          (territory-card territory)))]
+          (printout-templates/territory-card territory)))]
 
      [:div.no-print
       (map-interaction-help/view model)])))
