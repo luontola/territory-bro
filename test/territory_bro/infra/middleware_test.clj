a4e82dfd03d80a3e74458c4e7a155d9ad6d3bb6c
Esko Luontola <esko@luontola.fi>
2024-07-13 13:20:18 +0300

    ♻️ Use ring.mock.request
    Why:
    - Reduces the risk of creating an invalid request map.

--- test/territory_bro/infra/middleware_test.clj
+++ test/territory_bro/infra/middleware_test.clj
@@ -4,6 +4,7 @@
 
 (ns territory-bro.infra.middleware-test
   (:require [clojure.test :refer :all]
+            [ring.mock.request :as mock]
             [ring.util.http-response :as http-response]
             [ring.util.response :as response]
             [territory-bro.infra.middleware :as middleware]))
@@ -15,10 +16,8 @@
       (is (= {:status 200
               :headers {"Cache-Control" "private, no-cache"}
               :body ""}
-             (handler {:request-method :get
-                       :uri "/"})
-             (handler {:request-method :get
-                       :uri "/some/page"})))))
+             (handler (mock/request :get "/"))
+             (handler (mock/request :get "/some/page"))))))
 
   (testing "if response contains a custom cache-control header, that one is used"
     (let [handler (-> (constantly (-> (http-response/ok "")
@@ -27,10 +26,8 @@
       (is (= {:status 200
               :headers {"Cache-Control" "custom value"}
               :body ""}
-             (handler {:request-method :get
-                       :uri "/"})
-             (handler {:request-method :get
-                       :uri "/some/page"})))))
+             (handler (mock/request :get "/"))
+             (handler (mock/request :get "/some/page"))))))
 
   (testing "static assets are cached for one hour"
     (let [handler (-> (constantly (http-response/ok ""))
@@ -38,10 +35,8 @@
       (is (= {:status 200
               :headers {"Cache-Control" "public, max-age=3600, stale-while-revalidate=86400"}
               :body ""}
-             (handler {:request-method :get
-                       :uri "/favicon.ico"})
-             (handler {:request-method :get
-                       :uri "/assets/style.css"})))))
+             (handler (mock/request :get "/favicon.ico"))
+             (handler (mock/request :get "/assets/style.css"))))))
 
   (testing "static assets with a content hash are cached indefinitely"
     (let [handler (-> (constantly (http-response/ok ""))
@@ -49,10 +44,8 @@
       (is (= {:status 200
               :headers {"Cache-Control" "public, max-age=2592000, immutable"}
               :body ""}
-             (handler {:request-method :get
-                       :uri "/assets/style-4da573e6.css"})
-             (handler {:request-method :get
-                       :uri "/assets/image-28ead48996a4ca92f07ee100313e57355dbbcbf2.svg"})))))
+             (handler (mock/request :get "/assets/style-4da573e6.css"))
+             (handler (mock/request :get "/assets/image-28ead48996a4ca92f07ee100313e57355dbbcbf2.svg"))))))
 
   (testing "error responses are not cached"
     (let [handler (-> (constantly (http-response/not-found ""))
@@ -60,9 +53,6 @@
       (is (= {:status 404
               :headers {"Cache-Control" "private, no-cache"}
               :body ""}
-             (handler {:request-method :get
-                       :uri "/some/page"})
-             (handler {:request-method :get
-                       :uri "/assets/style.css"})
-             (handler {:request-method :get
-                       :uri "/assets/style-4da573e6.css"}))))))
+             (handler (mock/request :get "/some/page"))
+             (handler (mock/request :get "/assets/style.css"))
+             (handler (mock/request :get "/assets/style-4da573e6.css")))))))
