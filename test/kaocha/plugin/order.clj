28da2d17fe6954a6cfdb87de29daa9d49a5dbaff
Esko Luontola <esko@luontola.fi>
2024-10-03 15:05:46 +0300

    🔨 Do not skip tests which initialize the test data
    Why:
    - Previously if a browser tests failed, then Kaocha would rerun just the
      failed test and skip registration-test. But when the focused test
      depends on the test data created by registration-test, the test would
      never pass unless rerunning all tests manually.

--- test/kaocha/plugin/order.clj
+++ test/kaocha/plugin/order.clj
@@ -3,19 +3,79 @@
 ;; The license text is at http://www.apache.org/licenses/LICENSE-2.0
 
 (ns kaocha.plugin.order
-  (:require [kaocha.plugin :refer [defplugin]]))
+  (:require [clojure.test :refer :all]
+            [kaocha.plugin :refer [defplugin]]))
 
-(defn custom-order [test]
+(defn- custom-order [test]
   (get-in test [:kaocha.testable/meta :order] 0))
 
+(defn- do-not-skip-dependencies [tests]
+  (if (every? :kaocha.testable/skip tests)
+    tests
+    (map (fn [test]
+           (if (not (zero? (custom-order test)))
+             (dissoc test :kaocha.testable/skip)
+             test))
+         tests)))
+
 (defn custom-order-sort [test-plan]
-  (if-let [tests (:kaocha.test-plan/tests test-plan)]
+  (if-some [tests (:kaocha.test-plan/tests test-plan)]
     (assoc test-plan
            :kaocha.test-plan/tests
            (->> tests
                 (sort-by custom-order)
-                (map custom-order-sort)))
+                (map custom-order-sort)
+                (do-not-skip-dependencies)))
     test-plan))
+
+(deftest custom-order-sort-test
+  (testing "sorts the tests by :order metadata"
+    (let [sorted-order [{:kaocha.testable/meta {:order -2}}
+                        {:kaocha.testable/meta {:order -1}}
+                        {:kaocha.testable/meta {}}
+                        {:kaocha.testable/meta {:order 1}}]]
+      (is (= {:kaocha.test-plan/tests sorted-order}
+             (custom-order-sort
+              {:kaocha.test-plan/tests (shuffle sorted-order)})))))
+
+  (testing "the sorting is stable"
+    (let [original-order [{:kaocha.testable/desc "A"}
+                          {:kaocha.testable/desc "B"}
+                          {:kaocha.testable/desc "C"}]]
+      (is (= {:kaocha.test-plan/tests original-order}
+             (custom-order-sort
+              {:kaocha.test-plan/tests original-order})))))
+
+  (testing "sorts recursively"
+    (let [sorted-order [{:kaocha.testable/meta {:order 1}}
+                        {:kaocha.testable/meta {:order 2}}]]
+      (is (= {:kaocha.test-plan/tests [{:kaocha.test-plan/tests sorted-order}]}
+             (custom-order-sort
+              {:kaocha.test-plan/tests [{:kaocha.test-plan/tests (shuffle sorted-order)}]})))))
+
+  (testing "when a focused test depends on skipped tests, will not skip them"
+    (is (= {:kaocha.test-plan/tests [{:kaocha.testable/desc "setup-test"
+                                      :kaocha.testable/meta {:order -1}}
+                                     {:kaocha.testable/desc "skipped-test"
+                                      :kaocha.testable/skip true}
+                                     {:kaocha.testable/desc "focused-test"}]}
+           (custom-order-sort
+            {:kaocha.test-plan/tests [{:kaocha.testable/desc "setup-test"
+                                       :kaocha.testable/skip true
+                                       :kaocha.testable/meta {:order -1}}
+                                      {:kaocha.testable/desc "skipped-test"
+                                       :kaocha.testable/skip true}
+                                      {:kaocha.testable/desc "focused-test"}]}))))
+
+  (testing "will skip if all tests in the namespace are skipped"
+    (let [test-plan {:kaocha.test-plan/tests [{:kaocha.testable/desc "setup-test"
+                                               :kaocha.testable/skip true
+                                               :kaocha.testable/meta {:order -1}}
+                                              {:kaocha.testable/desc "skipped-test-1"
+                                               :kaocha.testable/skip true}
+                                              {:kaocha.testable/desc "skipped-test-2"
+                                               :kaocha.testable/skip true}]}]
+      (is (= test-plan (custom-order-sort test-plan))))))
 
 (defplugin kaocha.plugin/order
   (post-load [test-plan]
