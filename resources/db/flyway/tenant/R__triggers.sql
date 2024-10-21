a3fa0bf44593a617401121a9d4754baec48a2e25
Esko Luontola <esko@luontola.fi>
2024-10-21 15:45:15 +0300

    🗃️ Set explicit order for database triggers
    Why:
    - PostgreSQL runs the triggers in alphabetical order:
      "If multiple triggers of the same kind are defined for the same event,
       they will be fired in alphabetical order by name."
       https://www.postgresql.org/docs/current/sql-createtrigger.html
    - Previously, the validation was run after appending to change log,
      which is not what was intended.
    - Numbering the triggers will make the order more explicit.
    - Next we'll add a trigger for simplifying large geometries. It should
      run before the other steps, so number 1 was reserved for it.
    - The triggers are better put in a repeatable script, because they can
      be easily recreated the same as the functions they call. This will
      make refactoring easier.

---
+++ resources/db/flyway/tenant/R__triggers.sql
@@ -0,0 +1,55 @@
+-- territory
+
+create or replace trigger territory_2_validation
+    before insert or update
+    on territory
+    for each row
+execute function ${masterSchema}.validate_gis_change();
+
+create or replace trigger territory_3_change_log
+    after insert or update or delete
+    on territory
+    for each row
+execute function ${masterSchema}.append_gis_change_log();
+
+-- congregation_boundary
+
+create or replace trigger congregation_boundary_2_validation
+    before insert or update
+    on congregation_boundary
+    for each row
+execute function ${masterSchema}.validate_gis_change();
+
+create or replace trigger congregation_boundary_3_change_log
+    after insert or update or delete
+    on congregation_boundary
+    for each row
+execute function ${masterSchema}.append_gis_change_log();
+
+-- subregion
+
+create or replace trigger subregion_2_validation
+    before insert or update
+    on subregion
+    for each row
+execute function ${masterSchema}.validate_gis_change();
+
+create or replace trigger subregion_3_change_log
+    after insert or update or delete
+    on subregion
+    for each row
+execute function ${masterSchema}.append_gis_change_log();
+
+-- card_minimap_viewport
+
+create or replace trigger card_minimap_viewport_2_validation
+    before insert or update
+    on card_minimap_viewport
+    for each row
+execute function ${masterSchema}.validate_gis_change();
+
+create or replace trigger card_minimap_viewport_3_change_log
+    after insert or update or delete
+    on card_minimap_viewport
+    for each row
+execute function ${masterSchema}.append_gis_change_log();
---
+++ resources/db/flyway/tenant/V5__triggers_cleanup.sql
@@ -0,0 +1,11 @@
+drop trigger territory_change_log on territory;
+drop trigger territory_validation on territory;
+
+drop trigger congregation_boundary_change_log on congregation_boundary;
+drop trigger congregation_boundary_validation on congregation_boundary;
+
+drop trigger subregion_change_log on subregion;
+drop trigger subregion_validation on subregion;
+
+drop trigger card_minimap_viewport_change_log on card_minimap_viewport;
+drop trigger card_minimap_viewport_validation on card_minimap_viewport;
