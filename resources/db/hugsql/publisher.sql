53396eb53b6bb05317513d22a543e83f1d259250
Esko Luontola <esko@luontola.fi>
2024-11-06 15:35:08 +0200

    ⚰️ Remove unused get-publisher SQL query
    Why:
    - The query is no longer used after 715e3c7777b33fd80efb60c5917512665597057d

--- resources/db/hugsql/publisher.sql
+++ resources/db/hugsql/publisher.sql
@@ -4,12 +4,6 @@ from publisher
 where congregation = :congregation
 order by name;
 
--- :name get-publisher :? :1
-select congregation, id, name
-from publisher
-where congregation = :congregation
-  and id = :id;
-
 -- :name save-publisher :! :n
 insert into publisher (congregation, id, name)
 values (:congregation, :id, :name)
