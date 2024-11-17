3c2f78071384eea188b63a87e07d80a5faeeea2b
Esko Luontola <esko@luontola.fi>
2024-11-17 19:10:37 +0200

    ✨ Territory statistics
    Why:
    - The circuit overseers are always interested in knowing e.g. how many
      territories have been covered during the past 6 or 12 months, or since
      the last CO's visit. Calculate the most commonly requested numbers
      automatically. The users can calculate other statistics manually after
      we have implemented exporting the assignments as a spreadsheet.

--- src/territory_bro/ui/congregation_page.clj
+++ src/territory_bro/ui/congregation_page.clj
@@ -1,22 +1,81 @@
 (ns territory-bro.ui.congregation-page
-  (:require [clojure.string :as str]
+  (:require [clojure.math :as math]
+            [clojure.string :as str]
+            [territory-bro.domain.congregation :as congregation]
             [territory-bro.domain.dmz :as dmz]
             [territory-bro.ui.css :as css]
             [territory-bro.ui.hiccup :as h]
             [territory-bro.ui.html :as html]
             [territory-bro.ui.i18n :as i18n]
             [territory-bro.ui.info-box :as info-box]
-            [territory-bro.ui.layout :as layout]))
+            [territory-bro.ui.layout :as layout])
+  (:import (java.time LocalDate)
+           (java.time.temporal ChronoUnit)))
+
+(defn- average [numbers]
+  (when-not (empty? numbers)
+    (math/round (/ (apply + numbers)
+                   (count numbers)))))
+
+(defn- covered-after? [^LocalDate cutoff-date]
+  (fn [territory]
+    (when-some [last-covered (:territory/last-covered territory)]
+      (. cutoff-date isBefore last-covered))))
 
 (defn model! [request]
   (let [cong-id (get-in request [:path-params :congregation])
-        congregation (dmz/get-congregation cong-id)]
+        congregation (dmz/get-congregation cong-id)
+        territories (dmz/list-territories cong-id)
+        today (.toLocalDate (congregation/local-time congregation))
+        cutoff-6-months (.minusMonths today 6)
+        cutoff-12-months (.minusMonths today 12)
+        assignment-durations-in-days (into []
+                                           (comp (mapcat #(dmz/get-territory-assignment-history cong-id (:territory/id %)))
+                                                 (filter :assignment/end-date)
+                                                 (filter :assignment/covered-dates)
+                                                 (filter #(. cutoff-12-months isBefore (:assignment/end-date %)))
+                                                 (map #(max 1 (. ChronoUnit/DAYS between (:assignment/start-date %) (:assignment/end-date %)))))
+                                           territories)]
     {:congregation (select-keys congregation [:congregation/name])
-     :statistics {:congregation-boundary? (some? (dmz/get-congregation-boundary cong-id))
-                  :territories (count (dmz/list-territories cong-id))}
+     :getting-started {:congregation-boundary? (some? (dmz/get-congregation-boundary cong-id))
+                       :territories? (not (empty? territories))}
+     :statistics {:territories (count territories)
+                  :assigned-territories (count (filterv :territory/current-assignment territories))
+                  :covered-in-past-6-months (count (filterv (covered-after? cutoff-6-months) territories))
+                  :covered-in-past-12-months (count (filterv (covered-after? cutoff-12-months) territories))
+                  :average-assignment-days (average assignment-durations-in-days)}
      :permissions {:view-printouts-page (dmz/view-printouts-page? cong-id)
                    :view-settings-page (dmz/view-settings-page? cong-id)
-                   :gis-access (dmz/allowed? [:gis-access cong-id])}}))
+                   :view-statistics (dmz/allowed? [:view-congregation cong-id])
+                   :view-getting-started (dmz/allowed? [:gis-access cong-id])}}))
+
+
+(defn- count-and-percentage [count total]
+  (let [percentage (if (zero? total)
+                     0
+                     (math/round (* 100 (/ count total))))]
+    (str count " (" percentage "\u00A0%)")))
+
+(defn statistics [{:keys [statistics permissions]}]
+  (when (:view-statistics permissions)
+    (let [styles (:CongregationPage (css/modules))]
+      (info-box/view
+       {:title "Territory statistics"} ; TODO: i18n
+       (h/html
+        [:ul {:class (:statistics styles)}
+         [:li "Territories: " (:territories statistics)] ; TODO: i18n
+         [:li "Currently assigned: " ; TODO: i18n
+          (count-and-percentage (:assigned-territories statistics)
+                                (:territories statistics))]
+         [:li "Covered in past 6 months: " ; TODO: i18n
+          (count-and-percentage (:covered-in-past-6-months statistics)
+                                (:territories statistics))]
+         [:li "Covered in past 12 months: " ; TODO: i18n
+          (count-and-percentage (:covered-in-past-12-months statistics)
+                                (:territories statistics))]
+         (when-some [days (:average-assignment-days statistics)]
+           [:li "Average assignment duration: " days " days"])]))))) ; TODO: i18n
+
 
 (defn- checklist-item-status [completed?]
   (let [styles (:CongregationPage (css/modules))]
@@ -25,26 +84,24 @@
        :data-test-icon "✅"}
       {:data-test-icon "⏳"})))
 
-(defn getting-started [{:keys [statistics permissions]}]
-  (when (:gis-access permissions)
-    (let [styles (:CongregationPage (css/modules))
-          content (h/html
-                   [:ol {:class (:checklist styles)}
-                    [:li (checklist-item-status (:congregation-boundary? statistics))
-                     [:a {:href "/documentation#how-to-create-congregation-boundaries"}
-                      (i18n/t "GettingStarted.congregationBoundary")]]
-                    [:li (checklist-item-status (pos? (:territories statistics)))
-                     [:a {:href "/documentation#how-to-create-and-edit-territories"}
-                      (i18n/t "GettingStarted.territories")]]]
-                   [:p (-> (i18n/t "SupportPage.mailingListAd")
-                           (str/replace "<0>" "<a href=\"https://groups.google.com/g/territory-bro-announcements\" target=\"_blank\">")
-                           (str/replace "</0>" "</a>")
-                           (h/raw))])]
-      (h/html
-       [:aside {:class (:getting-started styles)}
-        (info-box/view
-         {:title (i18n/t "GettingStarted.title")}
-         content)]))))
+(defn getting-started [{:keys [getting-started permissions]}]
+  (when (:view-getting-started permissions)
+    (let [styles (:CongregationPage (css/modules))]
+      (info-box/view
+       {:title (i18n/t "GettingStarted.title")}
+       (h/html
+        [:ol {:class (:checklist styles)}
+         [:li (checklist-item-status (:congregation-boundary? getting-started))
+          [:a {:href "/documentation#how-to-create-congregation-boundaries"}
+           (i18n/t "GettingStarted.congregationBoundary")]]
+         [:li (checklist-item-status (:territories? getting-started))
+          [:a {:href "/documentation#how-to-create-and-edit-territories"}
+           (i18n/t "GettingStarted.territories")]]]
+        [:p (-> (i18n/t "SupportPage.mailingListAd")
+                (str/replace "<0>" "<a href=\"https://groups.google.com/g/territory-bro-announcements\" target=\"_blank\">")
+                (str/replace "</0>" "</a>")
+                (h/raw))])))))
+
 
 (defn view [{:keys [congregation permissions] :as model}]
   (let [styles (:CongregationPage (css/modules))]
@@ -68,6 +125,7 @@
                 " "
                 (i18n/t "SettingsPage.title")]])]]
       [:div.pure-u-1.pure-u-md-1-2
+       (statistics model)
        (getting-started model)]])))
 
 (defn view! [request]
--- test/territory_bro/ui/congregation_page_test.clj
+++ test/territory_bro/ui/congregation_page_test.clj
@@ -4,28 +4,77 @@
             [territory-bro.domain.congregation :as congregation]
             [territory-bro.domain.demo :as demo]
             [territory-bro.domain.dmz-test :as dmz-test]
+            [territory-bro.domain.publisher :as publisher]
+            [territory-bro.test.fixtures :refer :all]
             [territory-bro.test.testutil :as testutil :refer [replace-in]]
             [territory-bro.ui.congregation-page :as congregation-page]
-            [territory-bro.ui.html :as html]))
+            [territory-bro.ui.html :as html])
+  (:import (java.time LocalDate)))
 
 (def model
   {:congregation {:congregation/name "Example Congregation"}
-   :statistics {:congregation-boundary? false
-                :territories 0}
+   :getting-started {:congregation-boundary? false
+                     :territories? false}
+   :statistics {:territories 0
+                :assigned-territories 0
+                :covered-in-past-6-months 0
+                :covered-in-past-12-months 0
+                :average-assignment-days nil}
    :permissions {:view-printouts-page true
                  :view-settings-page true
-                 :gis-access true}})
+                 :view-statistics true
+                 :view-getting-started true}})
 (def model-with-congregation-boundary
-  (replace-in model [:statistics :congregation-boundary?] false true))
+  (replace-in model [:getting-started :congregation-boundary?] false true))
 (def model-with-territories
-  (replace-in model [:statistics :territories] 0 1))
+  (-> model
+      (replace-in [:getting-started :territories?] false true)
+      (replace-in [:statistics :territories] 0 2)
+      (replace-in [:statistics :assigned-territories] 0 1)
+      (replace-in [:statistics :covered-in-past-6-months] 0 1)
+      (replace-in [:statistics :covered-in-past-12-months] 0 1)
+      (replace-in [:statistics :average-assignment-days] nil 60)))
 (def demo-model
   {:congregation {:congregation/name "Demo Congregation"}
-   :statistics {:congregation-boundary? false
-                :territories 0}
+   :getting-started {:congregation-boundary? false
+                     :territories? false}
+   :statistics {:territories 0
+                :assigned-territories 0
+                :covered-in-past-6-months 0
+                :covered-in-past-12-months 0
+                :average-assignment-days nil}
    :permissions {:view-printouts-page true
                  :view-settings-page false
-                 :gis-access false}})
+                 :view-statistics true
+                 :view-getting-started false}})
+
+(use-fixtures :once (join-fixtures [(fixed-clock-fixture dmz-test/test-time)
+                                    (fn [f]
+                                      (binding [publisher/publishers-by-id (fn [_conn _cong-id]
+                                                                             nil)]
+                                        (f)))]))
+
+(defn- territory-covered-on [date]
+  (let [territory-id (random-uuid)]
+    [(assoc dmz-test/territory-defined
+            :territory/id territory-id)
+     (assoc dmz-test/territory-covered
+            :territory/id territory-id
+            :assignment/covered-date date)]))
+
+(defn- territory-and-assignment [start-date end-date]
+  (let [territory-id (random-uuid)]
+    [(assoc dmz-test/territory-defined
+            :territory/id territory-id)
+     (assoc dmz-test/territory-assigned
+            :territory/id territory-id
+            :assignment/start-date start-date)
+     (assoc dmz-test/territory-covered
+            :territory/id territory-id
+            :assignment/covered-date end-date)
+     (assoc dmz-test/territory-returned
+            :territory/id territory-id
+            :assignment/end-date end-date)]))
 
 (deftest model!-test
   (let [user-id (random-uuid)
@@ -45,12 +94,76 @@
             (is (= model-with-congregation-boundary (congregation-page/model! request)))))
 
         (testing "with some territories"
-          (testutil/with-events [dmz-test/territory-defined]
+          (testutil/with-events [dmz-test/territory-defined
+                                 dmz-test/territory-assigned
+                                 dmz-test/territory-covered
+                                 dmz-test/territory-returned
+                                 dmz-test/territory-defined2
+                                 (assoc dmz-test/territory-assigned :territory/id dmz-test/territory-id2)]
             (is (= model-with-territories (congregation-page/model! request)))))
 
         (testing "demo congregation"
           (let [request {:path-params {:congregation "demo"}}]
-            (is (= demo-model (congregation-page/model! request)))))))))
+            (is (= demo-model (congregation-page/model! request)))))
+
+        (testing "counts territories covered within 6 and 12 months"
+          (testutil/with-events (flatten [(territory-covered-on dmz-test/today)
+                                          (territory-covered-on (-> dmz-test/today (.minusMonths 6) (.plusDays 1)))
+                                          ;; -- 6 months --
+                                          (territory-covered-on (-> dmz-test/today (.minusMonths 6)))
+                                          (territory-covered-on (-> dmz-test/today (.minusMonths 12) (.plusDays 1)))
+                                          ;; -- 12 months --
+                                          (territory-covered-on (-> dmz-test/today (.minusMonths 12)))])
+            (is (= {:territories 5
+                    :covered-in-past-6-months 2
+                    :covered-in-past-12-months 4}
+                   (-> (congregation-page/model! request)
+                       :statistics
+                       (select-keys [:territories :covered-in-past-6-months :covered-in-past-12-months]))))))
+
+        (testing "calculates the average assignment duration:"
+          (let [average-assignment-days #(-> (congregation-page/model! request)
+                                             :statistics
+                                             :average-assignment-days)]
+            (testing "ignores covered but not returned"
+              (testutil/with-events [dmz-test/territory-defined
+                                     dmz-test/territory-assigned
+                                     dmz-test/territory-covered]
+                (is (nil? (average-assignment-days)))))
+
+            (testing "ignores returned but not covered"
+              (testutil/with-events [dmz-test/territory-defined
+                                     dmz-test/territory-assigned
+                                     dmz-test/territory-returned]
+                (is (nil? (average-assignment-days)))))
+
+            (testing "counts returned and covered"
+              (testutil/with-events [dmz-test/territory-defined
+                                     dmz-test/territory-assigned
+                                     dmz-test/territory-covered
+                                     dmz-test/territory-returned]
+                (is (= 60 (average-assignment-days)))))
+
+            (testing "calculates the average of all assignments"
+              (testutil/with-events (flatten [(territory-and-assignment (LocalDate/of 2000 1 10) (LocalDate/of 2000 1 15)) ; 5 days
+                                              (territory-and-assignment (LocalDate/of 2000 1 10) (LocalDate/of 2000 1 20))]) ; 10 days
+                (is (= 8 (average-assignment-days))))) ; (5 + 10) / 2 = 7.5 ~= 8
+
+            (testing "minimum duration is 1 day"
+              (testutil/with-events (territory-and-assignment (LocalDate/of 2000 1 10) (LocalDate/of 2000 1 10))
+                (is (= 1 (average-assignment-days))))
+              (testutil/with-events (territory-and-assignment (LocalDate/of 2000 1 10) (LocalDate/of 2000 1 11))
+                (is (= 1 (average-assignment-days))))
+              (testutil/with-events (territory-and-assignment (LocalDate/of 2000 1 10) (LocalDate/of 2000 1 12))
+                (is (= 2 (average-assignment-days)))))
+
+            (testing "includes assignments returned within the last 12 months"
+              (testutil/with-events (territory-and-assignment (-> dmz-test/today (.minusMonths 13))
+                                                              (-> dmz-test/today (.minusMonths 12)))
+                (is (nil? (average-assignment-days))))
+              (testutil/with-events (territory-and-assignment (-> dmz-test/today (.minusMonths 13))
+                                                              (-> dmz-test/today (.minusMonths 12) (.plusDays 1)))
+                (is (= 32 (average-assignment-days)))))))))))
 
 (deftest view-test
   (testing "full permissions"
@@ -60,6 +173,12 @@
              📍 Territories
              🖨️ Printouts
              ⚙️ Settings
+
+             {info.svg} Territory statistics
+             Territories: 0
+             Currently assigned: 0 (0 %)
+             Covered in past 6 months: 0 (0 %)
+             Covered in past 12 months: 0 (0 %)
 
              {info.svg} Getting started
              ⏳ Define the congregation boundary
@@ -75,9 +194,34 @@
            (-> (congregation-page/view (dissoc model :permissions))
                html/visible-text)))))
 
+(deftest statistics-test
+  (testing "hidden if the user can't see all territories"
+    (is (nil? (congregation-page/statistics (replace-in model [:permissions :view-statistics] true false)))))
+
+  (testing "new congregation"
+    (is (= (html/normalize-whitespace
+            "{info.svg} Territory statistics
+             Territories: 0
+             Currently assigned: 0 (0 %)
+             Covered in past 6 months: 0 (0 %)
+             Covered in past 12 months: 0 (0 %)")
+           (-> (congregation-page/statistics model)
+               html/visible-text))))
+
+  (testing "with some territories"
+    (is (= (html/normalize-whitespace
+            "{info.svg} Territory statistics
+             Territories: 2
+             Currently assigned: 1 (50 %)
+             Covered in past 6 months: 1 (50 %)
+             Covered in past 12 months: 1 (50 %)
+             Average assignment duration: 60 days")
+           (-> (congregation-page/statistics model-with-territories)
+               html/visible-text)))))
+
 (deftest getting-started-test
-  (testing "hidden if the user can't create territories themselves"
-    (is (nil? (congregation-page/getting-started (replace-in model [:permissions :gis-access] true false)))))
+  (testing "hidden if the user can't create territories"
+    (is (nil? (congregation-page/getting-started (replace-in model [:permissions :view-getting-started] true false)))))
 
   (testing "new congregation"
     (is (= (html/normalize-whitespace
