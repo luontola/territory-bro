17e8fe8437af2e4801d65b8daaf85021b32dacbb
Esko Luontola <esko@luontola.fi>
2024-11-30 18:54:08 +0200

    ✅ Update prod tests to match the new title format
    Why:
    - The congregation name was added to the title in b329d00bd9dcb9b5c8d3ea2299e2701936f6c15f,
      which broke these tests.

--- test/territory_bro/prod_test.clj
+++ test/territory_bro/prod_test.clj
@@ -8,14 +8,14 @@
   (testing "shared link"
     (let [response (http/get "https://territorybro.com/share/demo-5qs_xGRJTciwVAVkvuUX8A/1")]
       (is (= 200 (:status response)))
-      (is (str/includes? (:body response) "Territory 1 - Territory Bro"))
+      (is (str/includes? (:body response) "Territory 1 - Demo Congregation - Territory Bro"))
       (is (= ["https://territorybro.com/congregation/demo/territories/e6ab3fc4-6449-4dc8-b054-0564bee517f0"]
              (:trace-redirects response)))))
 
   (testing "shared link, old beta.territorybro.com domain"
     (let [response (http/get "https://beta.territorybro.com/share/demo-5qs_xGRJTciwVAVkvuUX8A/1")]
       (is (= 200 (:status response)))
-      (is (str/includes? (:body response) "Territory 1 - Territory Bro"))
+      (is (str/includes? (:body response) "Territory 1 - Demo Congregation - Territory Bro"))
       (is (= ["https://territorybro.com/share/demo-5qs_xGRJTciwVAVkvuUX8A/1"
               "https://territorybro.com/congregation/demo/territories/e6ab3fc4-6449-4dc8-b054-0564bee517f0"]
              (:trace-redirects response)))))
@@ -23,7 +23,7 @@
   (testing "QR code"
     (let [response (http/get "https://qr.territorybro.com/demo-5qs_xGRJTciwVAVkvuUX8A")]
       (is (= 200 (:status response)))
-      (is (str/includes? (:body response) "Territory 1 - Territory Bro"))
+      (is (str/includes? (:body response) "Territory 1 - Demo Congregation - Territory Bro"))
       (is (= ["https://territorybro.com/share/demo-5qs_xGRJTciwVAVkvuUX8A"
               "https://territorybro.com/congregation/demo/territories/e6ab3fc4-6449-4dc8-b054-0564bee517f0"]
              (:trace-redirects response))))))
